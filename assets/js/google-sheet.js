9fa36ec57bfc26bc22d3ecae104a477aa8b26a51
Luca <luca.lm@live.com>
2026-01-20 14:18:37 +0100

    utm source forms

--- assets/js/google-sheet.js
+++ assets/js/google-sheet.js
@@ -4,7 +4,9 @@ const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  const formData = new FormData(form);
+  formData.append("Page URL", window.location.href);
+  fetch(scriptURL, { method: 'POST', body: formData})
   .then(response => alert("Thank you! your form is submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
